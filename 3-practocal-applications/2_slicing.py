ab6fcc5d1aefe32ddd122fe0cdd3de852c3cb055
levan.tsutskiridze <levan.tsutskiridze@revelsystems.com>
2022-04-18 21:39:38 +0300

    adds 3.2

---
+++ 3-practocal-applications/2_slicing.py
@@ -0,0 +1,10 @@
+my_string = "Hello, world!"
+
+print(my_string[0])  # "H"
+print(my_string[7:10])  # "wor"
+print(my_string[:10])  # "Hello, wor"
+print(my_string[7:])  # "world!"
+print(my_string[:])  # "Hello, world!"
+print(my_string[-1])  # "!"
+print(my_string[-5:-1])  # "orld"
+
